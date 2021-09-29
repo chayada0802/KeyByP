53e00310e417a5d5c7d38cfb65df75f4c5672427
MAMAsoon <78579013+chayada0802@users.noreply.github.com>
2021-09-29 18:40:48 +0700

    เปลี่ยนฟุตเป็นนิ้ว

---
+++ UnitConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+namespace ConvernterExweek1
+{
+    class UnitConverter
+    {
+        int ratio;
+        public UnitConverter(int unitRatio)
+        {
+            ratio = unitRatio;
+        }
+        public int Convert (int unit)
+        {
+            return unit * ratio;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+            //behaviour อยู่ในmain
+        {
+            UnitConverter feetToInchCoverter = new UnitConverter(12);
+            UnitConverter mileToFeetConverter = new UnitConverter(5280);
+            Console.WriteLine(feetToInchCoverter.Convert(30));
+            Console.WriteLine(feetToInchCoverter.Convert(100));
+            Console.WriteLine(feetToInchCoverter.Convert(mileToFeetConverter.Convert(1)));
+
+        }
+    }
+}
