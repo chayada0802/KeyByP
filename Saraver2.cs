1535bc2ece9249f6c3ae82a944ae4d802dec3f43
MAMAsoon <78579013+chayada0802@users.noreply.github.com>
2021-09-29 09:53:35 +0700

    Add files via upload

---
+++ Saraver2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Sara
+{
+    class Program
+    {
+        static bool isVowel(char input)
+        {
+            return input == 'a' || input == 'e' ||
+                input == 'i' || input == 'o'
+                || input == 'u';
+        }
+        static void Main(string[] args)
+        {
+            int counter = 0;
+            Console.Write("Input name:");
+            string name = Console.ReadLine();
+            //"Chayada Vivattanakul"-->"chayada vivattanakul"
+            string lowerString = name.ToLower();
+            //"chayada vivattanakul"-->"CHAYADA VIVATTANAKUL"
+            string upperString = name.ToUpper();
+            //เปลี่ยนstringมาเป็นchar Array ทีละตัว
+            foreach(char value in lowerString.ToCharArray())
+            {
+                if (isVowel(value))
+                {
+                    counter++;
+                }
+            }
+            Console.WriteLine(counter);
+        }
+    }
+}
