4d486973b45b596d97027668e8baf26615ad873c
MAMAsoon <78579013+chayada0802@users.noreply.github.com>
2021-09-30 19:04:13 +0700

    ตัวอย่าง Overloading methods

---
+++ ตัวอย่าง Overloading methods.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ตัวอย่าง_Overloading_methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result1 = add(2, 3);
+            int result2 = add(1, 2, 3);
+
+            Console.WriteLine(result1);
+            Console.WriteLine(result2);
+        }
+
+        static int add(int number1, int number2)
+        {
+            return number1 + number2;
+        }
+
+        static int add(int number1, int number2, int number3)
+        {
+            return number1 + number2 + number3;
+        }
+    }
+}
