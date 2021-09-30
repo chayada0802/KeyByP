4109252a0356b4eb2cd76fb9a0a4b23b8e17a3d5
MAMAsoon <78579013+chayada0802@users.noreply.github.com>
2021-09-30 12:07:07 +0700

    ตัวอย่าง Value Type

---
+++ ValueType.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ValueType
+{
+    class Program
+    {
+        public struct Point { public int x; public int y; }
+        static void Main(string[] args)
+        {
+            Point p1 = new Point();
+            p1.x = 7;
+
+            Point p2 = p1; // copy
+
+            Console.WriteLine(p1.x);
+            Console.WriteLine(p2.x);
+
+            p1.x = 9;
+
+            Console.WriteLine(p1.x);
+            Console.WriteLine(p2.x);
+        }
+    }
+}
