08f0fcdb3121f0359bf05c7b47eb2f7e2c490d58
MAMAsoon <78579013+chayada0802@users.noreply.github.com>
2021-09-30 10:13:00 +0700

    แสดงชื่อสัตว์

---
+++ AnimalName.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Animal
+{
+    class Program
+    {
+        class Animal
+        {
+            public string name;
+            public string birthdate;
+            public string gender;
+        }
+            static void Main(string[] args)
+        {
+            Animal dog = new Animal();
+
+            dog.name = "Nhong";
+            dog.birthdate = "31 may 1991";
+            dog.gender = "male";
+
+            Console.WriteLine(dog.name);
+        }
+    }
+}
