43cac24d69521742f4d930bd13244f91c5d4f5fe
MAMAsoon <78579013+chayada0802@users.noreply.github.com>
2021-09-30 12:11:06 +0700

    ตัวอย่าง Reference Type

---
+++ Reference Type.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Reference_Type
+{
+    class Program
+    {
+        public class Point { public int x; public int y; }
+        static void Main(string[] args)
+        {
+            Point p1 = new Point();
+            p1.x = 7;
+
+            Point p2 = p1;
+
+            Console.WriteLine(p1.x);
+            Console.WriteLine(p2.x);
+
+            p1.x = 9;
+
+            Console.WriteLine(p1.x);
+            Console.WriteLine(p2.x);
+        }
+    }
+}
